d4d12d9738cbe4ec57e15acdcd3339cd74b409bd
Waldir <waliuxd@gmail.com>
2023-04-25 02:02:46 -0500

    prev

--- src/pages/Projects/ProjectsPage.jsx
+++ src/pages/Projects/ProjectsPage.jsx
@@ -1,34 +1,9 @@
-import React from "react";
-import styled from "styled-components";
-import "./ProjectsPage.scss";
+import React from 'react'
 
-const Button = styled.button`
-  display: inline-block;
-  color: palevioletred;
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
-  display: block;
-`;
-const TomatoButton = styled(Button)`
-  color: tomato;
-  border-color: tomato;
-`;
-
-const Projects = () => {
+const ProjectsPage = () => {
   return (
-    <div>
-      <Button>Normal Button</Button>
-      <Button as="a" href="#">
-        Link with Button styles
-      </Button>
-      <TomatoButton as="a" href="#">
-        Link with Tomato Button styles
-      </TomatoButton>
-    </div>
-  );
-};
+    <div>ProjectsPage</div>
+  )
+}
 
-export default Projects;
+export default ProjectsPage
